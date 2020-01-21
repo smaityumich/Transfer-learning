17e68476b03a2c0c4156584383ac13c142ec943a
smaityumich <56092728+smaityumich@users.noreply.github.com>
2020-01-21 10:17:29 -0500

    commit: 1/21/2020 10.17 AM ET

--- without_label/experiment1.py
+++ without_label/experiment1.py
@@ -14,12 +14,14 @@ import numpy as np
 import functools
 from classifier_no_label import *
 from data_generator import *
+import seaborn as sns
+import pandas as pd
 
 
 iter_step = 20
 n_target = 200
-alpha = np.arange(0.5, 2.5, step = 0.25)
-source_points = (2**np.array([0,1,2,3,4,5],dtype = int))*100
+alpha = np.arange(0.5, 2.5, step = 0.5)
+source_points = (2**np.array([0, 2, 4, 6],dtype = int))*100
 par = np.meshgrid(source_points, range(iter_step), alpha)
 par = np.array(par)
 par = par.reshape((3,-1))
@@ -56,6 +58,26 @@ error = map(_errors, classify)
 error = list(error)
 error = np.array(error)
 np.save('D:/GitHub/Tarnsfer-learning/without_label/result.npy', error)
+
+
+sns.set(style="whitegrid")
+tips = sns.load_dataset("tips")
+ax = sns.boxplot(x="day", y="total_bill", hue="smoker", data=tips, palette="Set3")
+
+n_source, alpha, gen_error = np.zeros(len(error)), np.zeros(len(error)), np.zeros(len(error))
+
+for i in range(len(error)):
+    par, gen_error[i] = error[i]
+    n_source[i], _, alpha[i] = par
+    
+    
+errors = pd.DataFrame(np.array([n_source, alpha, gen_error]).T, columns = ['n_source', '$\\alpha$', 'gen_error'])
+ax = sns.boxplot(x='n_source', y="gen_error", hue="$\\alpha$", data=errors, palette="Set3")  
+ax.set(xlabel = '$n_P$', ylabel = '$E \mathcal{E}_Q(\hat f)$')
+legend = '$\alpha$ with \n bandwidth $n^{-\alpha}$'
+ax.set_title('Plot for $E \mathcal{E}_Q(\hat f)$ with bandwidth $n_P^{-\\alpha}$')
+ax.get_figure().savefig('D:/GitHub/Tarnsfer-learning/without_label/errors.pdf')
+    
 
 
     
