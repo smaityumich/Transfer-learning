b2e5dd943397fbfff134c17a74c7c8cd0daa8c59
smaityumich <smaityumich>
2020-02-23 10:39:09 -0500

    modified:   SLURM-Job-Submitter.py

--- without-label-V2/SLURM-Job-Submitter.py
+++ without-label-V2/SLURM-Job-Submitter.py
@@ -15,20 +15,20 @@ log_dir = cwd + '.log'
 mkdir_path(log_dir)
 
 # Experiment 1
-n_targets = [50, 100, 200, 400, 800, 1600]
+n_sources = [50, 100, 400, 800, 1600]
 
 
 
 job_file = 'submit.sbat'
 
 
-for n_target in n_targets:
+for n_source in n_sources:
     os.system(f'touch {job_file}')
 
         
     with open(job_file,'w') as fh:
         fh.writelines("#!/bin/bash\n")
-        fh.writelines(f"#SBATCH --job-name=n_Q-{n_target}.job\n")
+        fh.writelines(f"#SBATCH --job-name=n_Q-{n_source}.job\n")
         fh.writelines('#SBATCH --nodes=1\n')
         fh.writelines('#SBATCH --cpus-per-task=10\n')
         fh.writelines('#SBATCH --mem-per-cpu=6gb\n')
@@ -37,7 +37,7 @@ for n_target in n_targets:
         fh.writelines("#SBATCH --mail-type=FAIL\n")
         fh.writelines("#SBATCH --mail-user=smaity@umich.edu\n")
         fh.writelines('#SBATCH --partition=standard\n')
-        fh.writelines(f"python3 unit_experiment.py 200 {n_target} 2500 0.5 0.8 1 5 100")
+        fh.writelines(f"python3 unit_experiment.py {n_source} 200 2500 0.5 0.8 1 5 100")
 
     os.system("sbatch %s" %job_file)
     os.system(f'rm {job_file}')
