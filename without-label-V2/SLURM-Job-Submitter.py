5f132a408172b48403cf65a92ad22c172170a62f
smaityumich <smaityumich>
2020-02-23 14:44:27 -0500

    modified:   SLURM-Job-Submitter.py

--- without-label-V2/SLURM-Job-Submitter.py
+++ without-label-V2/SLURM-Job-Submitter.py
@@ -14,12 +14,12 @@ mkdir_path(output_dir)
 log_dir = cwd + '.log'
 mkdir_path(log_dir)
 
-# Experiment 1
-n_sources = [50, 100, 400, 800, 1600]
-
 
 
 job_file = 'submit.sbat'
+
+# Experiment 1
+n_sources = np.array(range(1, 33))*50
 
 
 for n_source in n_sources:
@@ -28,7 +28,7 @@ for n_source in n_sources:
         
     with open(job_file,'w') as fh:
         fh.writelines("#!/bin/bash\n")
-        fh.writelines(f"#SBATCH --job-name=n_Q-{n_source}.job\n")
+        fh.writelines(f"#SBATCH --job-name=n_P-{n_source}.job\n")
         fh.writelines('#SBATCH --nodes=1\n')
         fh.writelines('#SBATCH --cpus-per-task=10\n')
         fh.writelines('#SBATCH --mem-per-cpu=6gb\n')
@@ -38,6 +38,31 @@ for n_source in n_sources:
         fh.writelines("#SBATCH --mail-user=smaity@umich.edu\n")
         fh.writelines('#SBATCH --partition=standard\n')
         fh.writelines(f"python3 unit_experiment.py {n_source} 200 2500 0.5 0.8 1 5 100")
+
+    os.system("sbatch %s" %job_file)
+    os.system(f'rm {job_file}')
+
+
+# Experiment 2
+n_targets = np.array(range(1, 33))*50
+
+
+for n_target in n_targets:
+    os.system(f'touch {job_file}')
+
+        
+    with open(job_file,'w') as fh:
+        fh.writelines("#!/bin/bash\n")
+        fh.writelines(f"#SBATCH --job-name=n_Q-{n_target}.job\n")
+        fh.writelines('#SBATCH --nodes=1\n')
+        fh.writelines('#SBATCH --cpus-per-task=10\n')
+        fh.writelines('#SBATCH --mem-per-cpu=6gb\n')
+        fh.writelines("#SBATCH --time=02:00:00\n")
+        fh.writelines("#SBATCH --account=stats_dept1\n")
+        fh.writelines("#SBATCH --mail-type=FAIL\n")
+        fh.writelines("#SBATCH --mail-user=smaity@umich.edu\n")
+        fh.writelines('#SBATCH --partition=standard\n')
+        fh.writelines(f"python3 unit_experiment.py 200 {n_target} 2500 0.5 0.8 1 5 100")
 
     os.system("sbatch %s" %job_file)
     os.system(f'rm {job_file}')
