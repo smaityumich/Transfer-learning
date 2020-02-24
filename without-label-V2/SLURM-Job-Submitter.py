bba7c28999ba3f8fcadae9efc06d77a921fbbe3c
Subha Maity <smaity@gl-login1.arc-ts.umich.edu>
2020-02-24 14:45:22 -0500

    modified:   SLURM-Job-Submitter.py

--- without-label-V2/SLURM-Job-Submitter.py
+++ without-label-V2/SLURM-Job-Submitter.py
@@ -19,7 +19,7 @@ mkdir_path(log_dir)
 job_file = 'submit.sbat'
 
 # Experiment 1
-n_sources = np.array(range(1, 33))*50
+n_sources = [3200, 6400, 12800, 25600]
 
 
 for n_source in n_sources:
@@ -44,7 +44,7 @@ for n_source in n_sources:
 
 
 # Experiment 2
-n_targets = np.array(range(1, 33))*50
+n_targets = [3200, 6400]
 
 
 for n_target in n_targets:
@@ -55,7 +55,7 @@ for n_target in n_targets:
         fh.writelines("#!/bin/bash\n")
         fh.writelines(f"#SBATCH --job-name=n_Q-{n_target}.job\n")
         fh.writelines('#SBATCH --nodes=1\n')
-        fh.writelines('#SBATCH --cpus-per-task=10\n')
+        fh.writelines('#SBATCH --cpus-per-task=30\n')
         fh.writelines('#SBATCH --mem-per-cpu=6gb\n')
         fh.writelines("#SBATCH --time=02:00:00\n")
         fh.writelines("#SBATCH --account=stats_dept1\n")
