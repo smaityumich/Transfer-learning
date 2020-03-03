9858174abefdf6c3127e84ecf9dfb359bd803c05
Subha Maity <smaity@gl-login1.arc-ts.umich.edu>
2020-03-03 11:41:17 -0500

    new file:   result.json

--- without-label-V2/SLURM-Job-Submitter-v2.py
+++ without-label-V2/SLURM-Job-Submitter-v2.py
@@ -6,7 +6,7 @@ job_file = 'submit.sbat'
 
 def expt(ns, nt, ntest, ps, pt, dist, d):
 
-    iteration = 200
+    iteration = 1
     for i in range(iteration):
         os.system(f'touch {job_file}')
 
@@ -17,7 +17,7 @@ def expt(ns, nt, ntest, ps, pt, dist, d):
             fh.writelines('#SBATCH --nodes=1\n')
             fh.writelines('#SBATCH --cpus-per-task=1\n')
             fh.writelines('#SBATCH --mem-per-cpu=1gb\n')
-            fh.writelines("#SBATCH --time=00:20:00\n")
+            fh.writelines("#SBATCH --time=02:20:00\n")
             fh.writelines("#SBATCH --account=stats_dept1\n")
             fh.writelines("#SBATCH --mail-type=NONE\n")
             fh.writelines("#SBATCH --mail-user=smaity@umich.edu\n")
@@ -27,10 +27,12 @@ def expt(ns, nt, ntest, ps, pt, dist, d):
         os.system("sbatch %s" %job_file)
         os.system(f'rm {job_file}')
 
-for nt in [25, 50, 100, 200, 400, 800, 1600, 3200, 6400]:
-    expt(2000, nt, 100, 0.5, 0.8, 2, 5)
+#for nt in [25, 50, 100, 200, 400, 800, 1600, 3200, 6400]:
+#    expt(2000, nt, 100, 0.5, 0.8, 2, 5)
 
 
 
-for ns in [25, 50, 100, 200, 400, 800, 1600, 3200, 6400, 12800]:
-    expt(ns, 100, 100, 0.5, 0.8, 2, 5)
+#for ns in [25, 50, 100, 200, 400, 800, 1600, 3200, 6400, 12800]:
+#    expt(ns, 100, 100, 0.5, 0.8, 2, 5)i
+
+expt(2000, 6400, 100, 0.5, 0.8, 2, 5)
