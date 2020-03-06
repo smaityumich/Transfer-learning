bffdb11c183498c488a5f395239d78f52523ba08
Subha Maity <smaity@gl-login1.arc-ts.umich.edu>
2020-03-06 10:23:00 -0500

    new file:   result-json/result-nq-dist-0.8.json

--- without-label-V2/SLURM-Job-Submitter-v2.py
+++ without-label-V2/SLURM-Job-Submitter-v2.py
@@ -6,7 +6,7 @@ job_file = 'submit.sbat'
 
 def expt(ns, nt, ntest, ps, pt, dist, d):
 
-    iteration = 200
+    iteration = 400
     for i in range(iteration):
         os.system(f'touch {job_file}')
 
@@ -27,8 +27,8 @@ def expt(ns, nt, ntest, ps, pt, dist, d):
         os.system("sbatch %s" %job_file)
         os.system(f'rm {job_file}')
 
-#for nt in [25, 50, 100, 200, 400, 800, 1600, 3200]:
-#    expt(100, nt, 100, 0.5, 0.8, 2, 5)
+for nt in [25, 50, 100, 200, 400, 800, 1600, 3200]:
+    expt(100, nt, 100, 0.5, 0.8, 0.8, 5)
 #for nt in [25, 50, 100, 200, 400, 800, 1600, 3200, 6400]:
 #    expt(100, nt, 100, 0.5, 0.8, 1, 5)
 
@@ -36,7 +36,7 @@ def expt(ns, nt, ntest, ps, pt, dist, d):
 #    expt(2*n, n, 100, 0.5, 0.8, 1, 5)
 
 
-for ns in [25, 50, 100, 200, 400, 800, 1600, 3200, 6400, 12800]:
-    expt(ns, 100, 100, 0.5, 0.8, 0.8, 5)
+#for ns in [25, 50, 100, 200, 400, 800, 1600, 3200, 6400, 12800]:
+#    expt(ns, 100, 100, 0.5, 0.8, 0.8, 5)
 
 #expt(2000, 6400, 100, 0.5, 0.8, 2, 5)
