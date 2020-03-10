c4a65ac68aa178dfa7d0305b31062d0b80e9ba35
smaityumich <smaityumich>
2020-03-10 18:57:24 -0400

    new file:   .data/n_source-100-n_target-100-n_test-100-prop_source-0.5-prop_target-0.8-dist-0.8-d-5-iter-0-normal-data.json new file:   .result/n_source-100-n_target-100-n_test-100-prop_source-0.5-prop_target-0.8-dist-0.8-d-5-iter-0-expt-QLabeled-normal-data.json

--- without-label-V2/.secondary/test_withLabelV3.py
+++ without-label-V2/.secondary/test_withLabelV3.py
@@ -1,48 +1,8 @@
-import numpy as np
+from dataGenerator import *
 from withLabelV3 import *
-import pickle
-from sklearn.pipeline import Pipeline
 
-
-xs, xt = np.random.normal(0, 1, (200,1)), np.random.normal(0, 1, (200,1))
-b = np.array([1])
-ls, lt = xs@b, xt@b
-ps, pt = 1/(1+np.exp(-ls)), 1/(1+np.exp(-lt))
-ys, yt = np.random.binomial(1, ps), np.random.binomial(1, pt)
-
-
-class DataHolder():
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def get(self):
-        return self.x, self.y
-
-data = DataHolder(xs, ys)
-
-
-xsp = pickle.dumps(xs)
-ysp = pickle.dumps(ys)
-
-
-bandwidths = np.linspace(0.1, 2, 2)
-param = {'bandwidth': bandwidths}
-#grid = GridSearchCV(WithLabelClassifier(), param, cv = 5)
- 
-cl = WithLabelClassifier() 
-
-
-#pipe = Pipeline(steps=[('estimator', WithLabelClassifier())])
-
-params_grid = {
-                'x_source': [xs],
-                'y_source': [ys],
-                'bandwidth': bandwidths
-                }
-
-l = ParameterGrid(params_grid)
-l = list(l)
-
-grid = CVGridSearch(cl, param, cv = 5)
+data = DataGenerator(5)
+xs, ys, _ = data._getData(200, 0.5, 2)
+xt, yt, _ = data._getData(100, 0.8, 2)
+cl = WithLabelOptimalClassifier()
+cl.fit(xs, ys, xt, yt)
--- without-label-V2/test_withLabelV3.py
+++
@@ -1,8 +0,0 @@
-from dataGenerator import *
-from withLabelV3 import *
-
-data = DataGenerator(5)
-xs, ys, _ = data._getData(200, 0.5, 2)
-xt, yt, _ = data._getData(100, 0.8, 2)
-cl = WithLabelOptimalClassifier()
-cl.fit(xs, ys, xt, yt)
