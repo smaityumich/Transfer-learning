1e38bcdd7dcc03c0f28a2a2a89e92be9645903e9
smaityumich <smaityumich>
2020-03-13 10:59:21 -0400

    modified:   SLURM-Job-Submitter-v2.py

--- without-label-V2/SLURM-Job-Submitter-v2.py
+++ without-label-V2/SLURM-Job-Submitter-v2.py
@@ -6,7 +6,7 @@ job_file = 'submit.sbat'
 
 def expt(ns, nt, ntest, ps, pt, dist, d):
 
-    iteration = 800
+    iteration = 1000
     for i in range(iteration):
         os.system(f'touch {job_file}')
 
@@ -22,7 +22,7 @@ def expt(ns, nt, ntest, ps, pt, dist, d):
             fh.writelines("#SBATCH --mail-type=NONE\n")
             fh.writelines("#SBATCH --mail-user=smaity@umich.edu\n")
             fh.writelines('#SBATCH --partition=standard\n')
-            fh.writelines(f"python3 unit_exptV3.py {ns} {nt} {ntest} {ps} {pt} {dist} {d} {i}")
+            fh.writelines(f"python3 unit_exptV2.py {ns} {nt} {ntest} {ps} {pt} {dist} {d} {i}")
 
         os.system("sbatch %s" %job_file)
         os.system(f'rm {job_file}')
@@ -36,7 +36,7 @@ def expt(ns, nt, ntest, ps, pt, dist, d):
 #    expt(2*n, n, 100, 0.5, 0.8, 1, 5)
 
 
-for ns in [20, 100, 500, 2500, 12500]:
-    expt(ns, 30, 100, 0.5, 0.9, 0.4, 4)
+for ns in [25, 50, 100, 200, 400, 800, 1600, 3200, 6400, 12800]:
+    expt(ns, 25, 100, 0.5, 0.9, 0.4, 4)
 
 #expt(2000, 6400, 100, 0.5, 0.8, 2, 5)
