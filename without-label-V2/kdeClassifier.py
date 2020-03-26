04c037212ba37d3ff6e0252a88891c717af1a9f5
smaityumich <smaityumich>
2020-03-26 15:34:08 -0400

    modified:   kdeClassifier.py

--- without-label-V2/kdeClassifier.py
+++ without-label-V2/kdeClassifier.py
@@ -40,9 +40,9 @@ class KDEClassifier(BaseEstimator, ClassifierMixin):
         
     def predict(self, X):
         if self.prop == 0:
-            return [0 for _ in x]
+            return [0 for _ in X]
         elif self.prop == 1:
-            return [1 for _ in x]
+            return [1 for _ in X]
         else:
             return self.classes_[np.argmax(self.predict_proba(X), 1)]
 
