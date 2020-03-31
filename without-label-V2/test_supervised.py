a6f17f77007346b1462298bd2402f80694b4513d
smaityumich <smaityumich>
2020-03-30 20:32:56 -0400

    modified:   SLURM-Job-Submitter-v2.py modified:   test_supervised.py

--- without-label-V2/test_supervised.py
+++ without-label-V2/test_supervised.py
@@ -5,9 +5,9 @@ from dataGenerator import *
 from mixtureClassifier import *
 import sys
 
-n_source, n_target, c = int(float(sys.argv[1])), int(float(sys.argv[2])), float(sys.argv[3])
+n_source, n_target, c, d, e = int(float(sys.argv[1])), int(float(sys.argv[2])), float(sys.argv[3]), float(sys.argv[4]), float(sys.argv[5])
 
-n_test = 100
+n_test = 250
 
 data = DataGenerator(4)
 xs, ys, _ = data._getData(n_source)
@@ -15,7 +15,7 @@ xt, yt, _ = data._getData(n_target, prop = 0.9)
 xtest, ytest, _ = data._getData(prop = 0.9)
 
 
-bandwidth = 2*c*(n_source+n_target)**(-1/6)
+bandwidth = c*(n_source+n_target)**(-1/6)
 cl = WithLabelOptimalClassifier(nodes = 1)
 cl.fit(x_source = xs, y_source = ys, x_target = xt, y_target = yt, bandwidth = bandwidth)
 y_pred = cl.predict(xtest)
@@ -23,7 +23,7 @@ error = np.mean((y_pred - ytest)**2)
 print(f'Both c:{c} error:{error}')
 
 
-bandwidth = c*(n_target)**(-1/6)
+bandwidth = e*(n_target)**(-1/6)
 cl = KDEClassifier(bandwidth = bandwidth)
 cl.fit(X = xt, y = yt)
 y_pred = cl.predict(xtest)
@@ -31,8 +31,8 @@ error = np.mean((y_pred - ytest)**2)
 print(f'Classical c:{c} error:{error}')
 
 cl = OptimalMixtureClassifier(nodes = 1)
-bandwidth_source = c*(n_source)**(-1/6)
-bandwidth_target = c*(n_target)**(-1/6)
+bandwidth_source = d*(n_source)**(-1/6)
+bandwidth_target = e*(n_target)**(-1/6)
 cl.fit(x_source = xs, y_source = ys, x_target = xt, y_target = yt, bandwidth_source=bandwidth_source, bandwidth_target=bandwidth_target)
 y_pred = cl.predict(xtest)
 error = np.mean((y_pred - ytest)**2)
